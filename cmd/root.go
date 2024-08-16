e1ed99aeacfb5b491dab31eef7371704faa72318
tokisuno <lucaswmei@gmail.com>
2024-08-16 10:35:46 -0400

    (feat) overwrite flag, writes to cwd dir, checks if .gitignore in cwd

--- cmd/root.go
+++ cmd/root.go
@@ -1,25 +1,96 @@
 package cmd
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
+	"slices"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "igen",
-	Short: "Generate .gitignore for project.",
-	Long: `Generates a .gitignore file for the project
-you're working on. This filetype must be supported by igen.`,
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Hello there!")
-    },
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+var Language string
+var Overwrite bool
+
+var rootCmd = &cobra.Command {
+	Use:   "igen",
+	Short: "Generate .gitignore for project.",
+	Long: `
+Generate a .gitignore file for the project you're working on! 
+!! This filetype must be supported by igen !!`,
+    Run: func(cmd *cobra.Command, args []string) {
+        var list []string
+
+        dir, err := os.Executable()
+
+        entries, err := os.ReadDir(filepath.Dir(dir) + "/cmd/templates"); check(err)
+
+        cwd, err := os.Getwd(); check(err)
+
+        for _, e := range entries {
+            list = append(list, e.Name())
+        }
+        if slices.Contains(list, Language) {
+            dat, err := os.ReadFile(filepath.Dir(dir) + "/cmd/templates/" + Language + "/.gitignore")
+            check(err)
+
+            if _, err := os.Stat(cwd + "/.gitignore") ; errors.Is(err, os.ErrNotExist) {
+                fmt.Println("file doesn't exist")
+                newGitignore(dat, cwd, Overwrite)
+            } else {
+                fmt.Println("file exists")
+                appendToGitignore(dat, cwd, Overwrite)
+            }
+        }
+	},
+}
+
+func newGitignore(dat []byte, cwd string, ow bool) {
+    fmt.Printf("%t", ow)
+    f, err := os.Create(cwd + "/.gitignore")
+    check(err)
+
+    defer f.Close()
+
+    w := bufio.NewWriter(f)
+    t, err := w.WriteString(string(dat))
+    check(err)
+    fmt.Printf("Wrote %d bytes\n", t)
+
+    f.Sync()
+    w.Flush()
+}
+
+func appendToGitignore(dat []byte, cwd string, ow bool) {
+    fmt.Printf("%t", ow)
+    f, err := os.OpenFile(cwd + "/.gitignore", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    check(err)
+
+    defer f.Close()
+
+    w := bufio.NewWriter(f)
+    t, err := w.WriteString(string(dat))
+    check(err)
+    fmt.Printf("Wrote %d bytes to existing .gitignore\n", t)
+
+    f.Sync()
+    w.Flush()
+}
+
+func init() {
+    rootCmd.Flags().BoolVarP(&Overwrite, "overwrite", "o", false, "Overwrite existing .gitignore")
+    rootCmd.Flags().StringVarP(&Language, "new", "n", "", "Requires Language arg")
+    rootCmd.MarkFlagRequired("new")
+}
+
 func Execute() {
     if err := rootCmd.Execute(); err != nil {
         fmt.Fprintln(os.Stderr, err)
@@ -27,16 +98,3 @@ func Execute() {
     }
 }
 
-// func init() {
-// 	// Here you will define your flags and configuration settings.
-// 	// Cobra supports persistent flags, which, if defined here,
-// 	// will be global for your application.
-//
-// 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.igen.yaml)")
-//
-// 	// Cobra also supports local flags, which will only run
-// 	// when this action is called directly.
-// 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-// }
-//
-//
--- cmd/version.go
+++ cmd/version.go
@@ -9,10 +9,10 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "0.1.0",
-	Long: `igen version -> 0.1.0`,
+	Short: "Displays current version",
+	Long: `Displays current version`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		fmt.Println("igen version -> 0.1.0")
 	},
 }
 
